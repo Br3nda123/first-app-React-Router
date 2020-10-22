7a4e7af2337c6b8b42b6b9ddb574156279425f98
Brandon Kunicki <brandon.kunicki@gmail.com>
2020-10-22 18:13:51 +0200

    Updates

---
+++ precache-manifest.3c00e04112baf0cd652c51c13cfec3c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb649bc1965362ed94bd",
+    "url": "/first-app-React-Router/static/css/main.a6bb9292.chunk.css"
+  },
+  {
+    "revision": "fb649bc1965362ed94bd",
+    "url": "/first-app-React-Router/static/js/main.fb649bc1.chunk.js"
+  },
+  {
+    "revision": "23e07efc8443c77d4233",
+    "url": "/first-app-React-Router/static/js/runtime~main.23e07efc.js"
+  },
+  {
+    "revision": "dbd52c1733de7c19f3ed",
+    "url": "/first-app-React-Router/static/js/2.dbd52c17.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/first-app-React-Router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/first-app-React-Router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/first-app-React-Router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "e19af7956ec33dec390d7b3c89b32446",
+    "url": "/first-app-React-Router/index.html"
+  }
+];
